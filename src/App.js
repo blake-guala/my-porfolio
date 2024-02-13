e852a5116254d12bcd3bac3f4b8115a193f99beb
iloham livingstone <ilohamlivingstonevictory@gmail.com>
2024-02-13 05:19:09 -0800

    styling

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
+import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
 import './App.css';
+import { Navbar } from './components/layout/Navbar';
+import { Services } from './components/layout/Services';
 
 function App() {
   return (
-    <div className="container">
-      portfolio
+    <div >
+      <Router>
+        <Navbar/>
+        <div className='container'>
+          <Routes>
+            <Route path='/services' element={<Services/>} />
+          </Routes>
+        </div>
+      </Router>
     </div>
   );
 }
--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -2,6 +2,21 @@ import React from 'react'
 
 export const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <nav className='nav-custom'>
+        <ul className='ul-custom'>
+            <li>
+            <a  href="!#">hire me</a>
+            </li>
+            <li>
+            <a  href="/services">Service</a>
+            </li>
+            <li>
+            <a  href="!#">Contact</a>
+            </li>
+            <li>
+            <a   href="/">Home</a>
+            </li>
+        </ul>
+    </nav>
   )
 }
