a7fedaaf19fdb193802ec16cd72786245aa3fa61
iloham livingstone <ilohamlivingstonevictory@gmail.com>
2024-02-13 08:36:17 -0800

    navbar css

--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -3,19 +3,20 @@ import React from 'react'
 export const Navbar = () => {
   return (
     <nav className='nav-custom'>
+        <h3 className='header'>Iheama</h3>
         <ul className='ul-custom'>
-            <li>
-            <a  href="!#">hire me</a>
-            </li>
-            <li>
-            <a  href="/services">Service</a>
-            </li>
-            <li>
-            <a  href="!#">Contact</a>
-            </li>
-            <li>
+          <li className='li-custom'>
             <a   href="/">Home</a>
-            </li>
+          </li>
+          <li className='li-custom'>
+            <a  href="/services">Service</a>
+          </li>
+          <li className='li-custom'>
+            <a  href="!#">Contact</a>
+          </li>
+          <li className='li-custom hire'>
+            <a  href="!#">hire me</a>
+          </li>
         </ul>
     </nav>
   )
