97f4e7e06a1813e3058634d8390e2af2876b3b54
iloham livingstone <ilohamlivingstonevictory@gmail.com>
2024-02-15 03:12:02 -0800

    added icons etc

--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -5,16 +5,16 @@ export const Navbar = () => {
 
   return (
     <nav className='nav-custom ' >
-        <h3 className='header'>IHEAMA</h3>
+        <h3 className='header'>Iloham</h3>
         <ul className='ul-custom'>
           <li className='li-custom'>
             <a   href="/">Home</a>
           </li>
           <li className='li-custom'>
-            <a  href="/services">Service</a>
+            <a  href="#projects">Projects</a>
           </li>
           <li className='li-custom'>
-            <a  href="!#">Contact</a>
+            <a  href="!#">About</a>
           </li>
           <li className='li-custom hire'>
             <a   href="!#">Hire me <i className="fas fa-angle-double-right" style={{ fontSize: '0.6rem',fontWeight: 'bolder' }}></i></a>
--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import svg from './web.svg'
-import drip from './drip.svg'
+// import drip from './drip.svg'
 
 export const Home = () => {
   return (
@@ -8,20 +8,78 @@ export const Home = () => {
         <div className="row background">
             <section className="col-sm">
                 <h5>Hello there,</h5>
-                <h1> i am Iloham livingstone.</h1>
+                <h1> i am Iloham livingstone iheama.</h1>
                 <p className='green'> FRONT END DEVELOPER</p>
             </section>
             <section className="col-sm">
                 <img className='img' src={svg} alt="backround" />
             </section>
         </div>
-        <section>
+        {/* <section>
             <img className='drip' src={drip} alt="drip..." />
-        </section>
+        </section> */}
+        <div className='skills'>
         <section>
-            hhh
+            <div className='div-projects'>
+            <h3 className='projects'> Projects(some) </h3>
+            </div>
         </section>
-        
+        <div className='row '>
+            <section className="col-sm section-box left">
+                {/* <div> */}
+                    <h3>Contact Keeper <i className="fa fa-address-book"></i></h3>
+                    <p>An app that stores contacts, users have to login first or register before use</p>
+                {/* </div> */}
+                {/* <div> */}
+                    <ul className='listed'>
+                    <h5 className='skill'>skills</h5>
+                        <li>React <i className="fa-brands fa-react"></i></li>
+                        <li>Nodejs <i className="fa-brands fa-node-js"></i></li>
+                    </ul>
+                    <div className='div-a'>
+                        <a href="#!">Source code <i className="fas fa-file-code"></i></a>
+                    {/* </div> */}
+                </div>
+            </section>
+            <section className="col-sm section-box right">
+                <h3>Github Finder <i className="fa-brands fa-github"></i></h3>
+                <p>An app that searches for users in github</p>
+                <ul className='listed'>
+                <h5 className='skill'> skills</h5>
+                    <li>React <i className="fa-brands fa-react"></i></li>
+                    <li>GithubApi <i className="fa-brands fa-github"></i></li>
+                </ul>
+                <div className='div-a'>
+                <a href="#!">Source code <i className="fas fa-file-code"></i></a>
+                </div>
+            </section>
+        </div>
+        </div>
+        <div>
+        <section className=" section-box bottom">
+                <h3>IT Logger App <i className="fas fa-hard-hat"></i></h3>
+                <p>An app used by IT depart to track changes made. has CRUD 
+                    functionalities</p>
+                <ul className='listed'>
+                <h5 className='skill'> skills</h5>
+                    <li>React <i className="fa-brands fa-react"></i></li>
+                    <li>JSON Server <i className="fa fa-file" ></i> <i className="fa-solid fa-server"></i></li>
+                </ul>
+                <div className='div-a'>
+                <a href="#!">Source code <i className="fas fa-file-code"></i></a>
+                </div>
+            </section>
+        </div>
+        <section>
+            <h3 className='about'>skills <i className="fa-solid fa-code"></i></h3>
+            <ul>
+                <li><i className="fab fa-js"></i></li>
+                <li><i className="fa-brands fa-github"></i></li>
+                <li><i className="fa-brands fa-react"></i></li>
+                <li><i className="fa-brands fa-bootstrap"></i></li>
+                <li><i className="fab fa-html5"></i></li>
+            </ul>
+        </section>
     </div>
   )
 }
