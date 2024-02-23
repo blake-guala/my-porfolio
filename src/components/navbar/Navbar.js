8df5ddb2f58430af8514ec61bb3fee74ea4028a3
iloham livingstone <ilohamlivingstonevictory@gmail.com>
2024-02-23 03:32:23 -0800

    adding media at 761px

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -5,11 +5,16 @@ export const Navbar = () => {
   // eslint-disable-next-line
   const [modal, setModal] = useState(false)
 
+  const onToggle = (e) => {
+    setModal(!modal)
+    
+  } 
+
   return (
     <header>
     <nav className={styles.navCustom} >
         <h3 className={styles.header}>ILOHAM</h3>
-        <ul className={styles.ul}>
+        <ul className={`${styles.ul} ${modal && styles.active}`}>
           <li className={styles.liCustom}>
             <a className='a'  href="/">Home</a>
           </li>
@@ -22,10 +27,10 @@ export const Navbar = () => {
           <li className={`${styles.liCustom} `}>
             <a   href="!#">contact</a>
           </li>
-          <li className={`${styles.liCustom} ${styles.mobile_button}`} >
-            <i className="fas fa-bars"></i>
-          </li>
         </ul>
+        <div onClick={onToggle} className={`${styles.liCustom} ${styles.mobile_button}`}>
+          { modal ? <i className="fa-solid fa-x"></i> :  <i  className="fas fa-bars fa-lg"></i> }
+        </div>
     </nav>
     </header>
   )
