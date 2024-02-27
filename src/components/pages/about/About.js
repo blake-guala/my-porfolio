e222c0620f65540cb5391601163c6337c7508308
iloham livingstone <ilohamlivingstonevictory@gmail.com>
2024-02-27 07:26:28 -0800

    live button and media 311px

--- src/components/pages/about/About.js
+++ src/components/pages/about/About.js
@@ -3,7 +3,7 @@ import styles from './About.module.css'
 
 export const About = () => {
   return (
-    <div style={{marginTop: '4rem'}}>
+    <div style={{marginTop: '4rem', marginLeft: 'auto', marginRight: 'auto', display: 'block'}}>
             <div className= {` ${styles.sectionBox} ${styles.bottom} ${styles.info} `} >
             <h4 className={styles.infoHeader}>About me</h4>
             <p>As an Developer, mathematician and Medical scientist my first love will always be solving problems
