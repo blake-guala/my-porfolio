cbc18d19d0a704d9f243d3cd59cf3e3c845d28aa
iloham livingstone <ilohamlivingstonevictory@gmail.com>
2024-03-04 09:17:13 -0800

    .

--- src/components/pages/getintouch/GetInTouch.js
+++ src/components/pages/getintouch/GetInTouch.js
@@ -6,77 +6,77 @@ import { toast } from 'react-toastify'
 
 export const GetInTouch = () => {
 
-    const form = useRef();
+  const form = useRef();
 
-    const sendEmail = (e) => {
-        e.preventDefault();
+  const sendEmail = (e) => {
+    e.preventDefault();
 
-         const id = toast.loading("Sending...", {
-            theme: "dark"
-         })
+    const id = toast.loading("Sending...", {
+      theme: "dark"
+    })
 
-         
 
-    
-        emailjs
-          .sendForm('service_o2j7wbh', 'template_dex67mp', form.current, {
-            publicKey: 'HJclrO3DeJ5EijvTO',
+
+
+    emailjs
+      .sendForm('service_o2j7wbh', 'template_dex67mp', form.current, {
+        publicKey: 'HJclrO3DeJ5EijvTO',
+      })
+      .then(
+        () => {
+          console.log('SUCCESS!');
+          toast.dismiss(id.current);
+          toast.success('Sent', {
+            position: "top-right",
+            autoClose: 5000,
+            hideProgressBar: false,
+            closeOnClick: false,
+            pauseOnHover: true,
+            draggable: true,
+            progress: undefined,
+            theme: "dark",
+            // transition: Bounce,
           })
-          .then(
-            () => {
-              console.log('SUCCESS!');
-              toast.dismiss(id.current);
-              toast.success('Sent', {
-                position: "top-right",
-                autoClose: 5000,
-                hideProgressBar: false,
-                closeOnClick: false,
-                pauseOnHover: true,
-                draggable: true,
-                progress: undefined,
-                theme: "dark",
-                // transition: Bounce,
-                })
-                form.current.reset()
-            },
-            (error) => {
-              console.log('FAILED...', error.text);
-              toast.dismiss(id.current);
-              toast.error('An error occurred!', {
-                position: "top-right",
-                autoClose: 5000,
-                hideProgressBar: false,
-                closeOnClick: true,
-                pauseOnHover: true,
-                draggable: true,
-                progress: undefined,
-                theme: "dark",
-                });
-            },
-          );
-      };
+          form.current.reset()
+        },
+        (error) => {
+          console.log('FAILED...', error.text);
+          toast.dismiss(id.current);
+          toast.error('An error occurred !', {
+            position: "top-right",
+            autoClose: 5000,
+            hideProgressBar: false,
+            closeOnClick: true,
+            pauseOnHover: true,
+            draggable: true,
+            progress: undefined,
+            theme: "dark",
+          });
+        },
+      );
+  };
   return (
     <div id='hire'>
-        <div className={`${style.sectionBox} ${style.bottom} ${style.info} ${style.form}`} >
-            <h4 className={style.infoHeader}>Leave a Message</h4>
-            <form ref={form} onSubmit={sendEmail} className={`${style.container} ${style.formCustom}`} >
-            <hr />
-                <div className="mb-3">
-                    <label htmlFor="name" className="form-label">Name</label>
-                    <input type="text"  name='user_name' className={` ${style.inputCustom}`} placeholder='your name' required/>
-                </div>
-                <div className="mb-3">
-                    <label htmlFor="email" className="form-label">Email</label>
-                    <input type="email" name='user_email' className={` ${style.inputCustom}`}   placeholder='your email' required />
-                </div>
-                <div className="mb-3">
-                    <label htmlFor="message" className="form-label ">Message</label>
-                    <textarea type="text" name='message' className={` ${style.inputCustom} ${style.messageBody} `}  
-                    placeholder='write your message...' required />
-                </div>
-                <button type="submit " className={`btn btn-primary ${style.buttonCustom}`}>Send</button>
-            </form>
-        </div>
+      <div className={`${style.sectionBox} ${style.bottom} ${style.info} ${style.form}`} >
+        <h4 className={style.infoHeader}>Leave a Message</h4>
+        <form ref={form} onSubmit={sendEmail} className={`${style.container} ${style.formCustom}`} >
+          <hr />
+          <div className="mb-3">
+            <label htmlFor="name" className="form-label">Name</label>
+            <input type="text" name='user_name' className={` ${style.inputCustom}`} placeholder='your name' required />
+          </div>
+          <div className="mb-3">
+            <label htmlFor="email" className="form-label">Email</label>
+            <input type="email" name='user_email' className={` ${style.inputCustom}`} placeholder='your email' required />
+          </div>
+          <div className="mb-3">
+            <label htmlFor="message" className="form-label ">Message</label>
+            <textarea type="text" name='message' className={` ${style.inputCustom} ${style.messageBody} `}
+              placeholder='write your message...' required />
+          </div>
+          <button type="submit " className={`btn btn-primary ${style.buttonCustom}`}>Send</button>
+        </form>
+      </div>
     </div>
   )
 }
